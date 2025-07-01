c14763e23669117ea191a4c8b37f925b18a87666
Noah <nbelman1@gmail.com>
2025-06-30 22:35:38 -0400

    Run script, fix errors, annotate code. Add my own examples.

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -18,15 +18,44 @@ Use console.log() to clearly show the before-and-after type conversions.
 
 */
 
+const { use } = require("react");
 
-let result = "5" - 2;
-console.log("The result is: " + result);
 
-let isValid = Boolean("false");
+
+// ----- PART 1 -----
+
+let result = Number("5") - 2;
+console.log("The result is: " + result); 
+// The original code ran, but could create errors if using addition.
+// Typecasting 5 to a number ensures viability across different operation types.
+
+let isValid = false; 
 if (isValid) {
     console.log("This is valid!");
 }
+// Typecasting "false" to a boolean results in true. Since false is already a 
+// boolean value, removing the typecasting removes ambiguity and creates 
+// working code. 
 
-let age = "25";
+let age = Number("25");
 let totalAge = age + 5;
 console.log("Total Age: " + totalAge);
+// When addition is used with a string, it's concatenated instead of added. 
+// Typecasting the string to a number causes the plus sign to add instead of concatenate. 
+
+
+
+// ----- PART 2 -----
+
+let numOfHundreds = "5";
+let numOfTens = "2";
+let numOfOnes = 7;
+let grandTotal = numOfHundreds + numOfTens + numOfOnes;
+console.log(grandTotal);
+// Using implicit type conversion allows us to take advantage of string concatenation.
+
+
+let userName = undefined;
+let userNameValid = Boolean(userName);
+console.log("userName's value is: " + userName);
+console.log("userName is this type of variable: " + typeof(userNameValid));
